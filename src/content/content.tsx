3804b3ab2ee234f79949532ef7c33e53af8bfeef
Sai Arvind Budaraju <23544799+saiarvindb@users.noreply.github.com>
2023-10-29 18:16:14 -0400

    Added translation to english (#4)
    Co-authored-by: Sai Arvind Budaraju <saiarvind@budaraju.com>

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -7,11 +7,12 @@ const main : () => void = () =>
 {
 	const root : HTMLDivElement = document.createElement("div");
 	root.id = "pictorpret";
-	root.style.position = "absolute";
+	root.style.position = "fixed";
 	root.style.top = "0";
 	root.style.left = "0";
 	root.style.width = "100%";
 	root.style.height = "100%";
+	root.style.zIndex = "2147483647";
 	
 	const Ext : FC = () =>
 	{
